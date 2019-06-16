9c9a90f522c98a125a0c5b56295043927387310b
Eloisa Morais <eloisa.smorais@gmail.com>
2019-06-16 01:04:27 -0300

    Adiciona funçao escolheTipo, altera assinatura da funçao de calculaArea para converteArea

--- conversor.c
+++ conversor.c
@@ -3,11 +3,12 @@
 
 int menu() {
     int opc;
-    printf("Escolha uma opção: ");
+    printf("Escolha uma opção\n");
     printf("1) Sobre o programa - instruções e outras informações\n");
     printf("2) Realizar conversão\n");
     printf("3) Descrição - conceitos, histórico, definição e processo de conversão\n");
-    printf("4) Sair");
+    printf("4) Sair\n");
+    printf("Digite a opção escolhida: ");
     scanf("%d", &opc);
     while (opc < 0 || opc > 4) { //Loop enquanto uma opção inválida for digitada 
         printf("Opção inválida, digite novamente: ");
@@ -25,20 +26,42 @@ void imprimeDescricao() {
 
 }
 
-void calculaArea() {
+int escolheTipo() {
+    int tipo;
+    printf("1) Quilômetro quadrado (km²)\n");
+    printf("2)Hectare (ha)\n");
+    printf("3) Metro quadrado(m²)\n");
+    printf("4) Centímetro quadrado (cm²)\n");
+    printf("5) Milímetro quadrado (mm²)\n");
+    printf("6) Micrometro quadrado\n");
+    printf("7) Nanômetro quadrado\n");
+    scanf("%d", &tipo);
+
+    return tipo;
+}
+
+void converteArea (int t1, int t2) {
 
 }
 
 int main() {
     setlocale(LC_ALL, "Portuguese");
     int opcao;    
+    char tipo1, tipo2;
 
     opcao = menu(); //Atribui à variavel o valor retornado da função
 
     //Chama as funções de acordo com a opção escolhida
     if (opcao == 1) imprimeSobre(); 
-    if (opcao == 2) calculaArea();
+    if (opcao == 2) {
+        printf("CONVERTER\n");
+        tipo1 = escolheTipo();
+        tipo2 = escolheTipo();
+        converteArea(tipo1, tipo2);
+        
+    }
     if (opcao == 3) imprimeDescricao();
+    if (opcao == 4) return 0;
  
     return 0;
 }
